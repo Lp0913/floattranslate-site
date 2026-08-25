f5488c66ef543f8ba41e8c770a2413f1d511405e
Lp0913 <114653927+Lp0913@users.noreply.github.com>
2026-08-25 14:22:26 +0800

    Add app entry point

---
+++ Sources/FloatTranslate/FloatTranslateApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct FloatTranslateApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
+    var body: some Scene {
+        Settings {
+            EmptyView()
+        }
+    }
+}
